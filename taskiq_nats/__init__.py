079454410de09b494939ff0cf4eedb5e408a2b64
Pavel Kirilin <win10@list.ru>
2023-05-30 03:46:13 +0400

    Initial jetstream support.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- taskiq_nats/__init__.py
+++ taskiq_nats/__init__.py
@@ -5,6 +5,6 @@ This package contains broker that
 uses NATS as a message queue.
 """
 
-from taskiq_nats.broker import NatsBroker
+from taskiq_nats.broker import NatsBroker, NatsJetStreamBroker
 
-__all__ = ["NatsBroker"]
+__all__ = ["NatsBroker", "NatsJetStreamBroker"]
