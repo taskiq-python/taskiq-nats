6bc5de7f774c470613a8317777c083e210c67ee0
Pavel Kirilin <win10@list.ru>
2023-09-29 18:19:19 +0400

    Fixed type-formatting.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- tests/utils.py
+++ tests/utils.py
@@ -1,14 +1,16 @@
+from typing import Union
+
 from taskiq import AckableMessage, AsyncBroker
 
 
-async def read_message(broker: AsyncBroker) -> bytes | AckableMessage:
+async def read_message(broker: AsyncBroker) -> Union[bytes, AckableMessage]:
     """
     Read signle message from the broker's listen method.
 
     :param broker: current broker.
     :return: firs message.
     """
-    msg: bytes | AckableMessage = b"error"
+    msg: Union[bytes, AckableMessage] = b"error"
     async for message in broker.listen():
         msg = message
         break
